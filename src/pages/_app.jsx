9c6d1cd128fe2b24af9dbf01af4a831289f60133
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-09-07 11:59:05 -0300

    Refacotr favicom

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -18,7 +18,7 @@ const App = ({ Component, pageProps }) => {
     <>
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/jimicon.png' />
       </Head>
       <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
         <NavBar />
